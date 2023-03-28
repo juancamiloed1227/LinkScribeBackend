9fb4f2830a766d72259179bc47fedb800515b22a
Juan Camilo <juancamilo12.jced@gmail.com>
2023-03-28 10:19:17 -0500

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,9 +1,14 @@
 import { Sequelize } from "sequelize";
+import * as dotenv from 'dotenv';
+dotenv.config()
+
+console.log(process.env.DB_USERNAME)
 
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
   host: process.env.DB_HOST,
-  dialect: 'postgres',
-  logging: false
+    port: 5432,
+    logging: console.log,
+    dialect: 'postgres',
 });
 
 export {
